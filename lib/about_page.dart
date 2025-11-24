6ab42af6660a59ec3aa557ce85adb3456d2dcc9f
Aryaan3123 <aryaandeeba@gmail.com>
2025-11-24 13:21:12 +0000

    Create AboutPage to use PageLayout Header & Footer, add text and styling

--- lib/about_page.dart
+++ lib/about_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:union_shop/page_layout.dart';
 
 class AboutPage extends StatelessWidget {
   const AboutPage({super.key});
@@ -9,14 +10,30 @@ class AboutPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('About Page'),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () => navigateToHome(context),
-          child: const Text('Go to Home'),
+    return PageLayout(
+      child: Container(
+        child: const Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Padding(
+              padding: EdgeInsets.all(40.0),
+              child: Text(
+                'About Us',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 50,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Welcome to Union Shop! We are committed to providing the best products and services to our customers. Our mission is to create a seamless shopping experience for everyone.',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 18),
+            ),
+          ],
         ),
       ),
     );
