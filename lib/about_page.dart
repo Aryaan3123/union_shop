fcbad0fbd330bebf574464dd51b942fd2c99d703
Aryaan3123 <aryaandeeba@gmail.com>
2025-11-24 13:30:22 +0000

    Update AboutPage content format and styling for improved user experience

--- lib/about_page.dart
+++ lib/about_page.dart
@@ -28,10 +28,16 @@ class AboutPage extends StatelessWidget {
               ),
             ),
             SizedBox(height: 20),
-            Text(
-              'Welcome to Union Shop! We are committed to providing the best products and services to our customers. Our mission is to create a seamless shopping experience for everyone.',
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 18),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 400.0),
+              child: Text(
+                'Welcome to Union Shop!\n\n We’re dedicated to giving you the very best University branded products, with a range of clothing and merchandise available to shop all year round! We even offer an exclusive personalisation service!\n\n All online purchases are available for delivery or instore collection! \n\n We hope you enjoy our products as much as we enjoy offering them to you. If you have any questions or comments, please don’t hesitate to contact us at hello@upsu.net. \n\n Happy shopping! \n\n The Union Shop & Reception Team​​​​​​​​​,',
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Colors.grey,
+                )
+              ),
             ),
           ],
         ),
