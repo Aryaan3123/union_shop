cba362e164377b61e9bd87c03301f187fcd4f02d
Aryaan3123 <aryaandeeba@gmail.com>
2025-12-01 16:19:19 +0000

    Fix overflow error in About Page for mobile

--- lib/pages/about_page.dart
+++ lib/pages/about_page.dart
@@ -13,11 +13,11 @@ class AboutPage extends StatelessWidget {
     return PageLayout(
       child: Container(
         color: Colors.white,
-        child: const Column(
+        child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Padding(
+            const Padding(
               padding: EdgeInsets.all(40.0),
               child: Text(
                 'About Us',
@@ -30,8 +30,9 @@ class AboutPage extends StatelessWidget {
             ),
             SizedBox(height: 20),
             Padding(
-              padding: EdgeInsets.symmetric(horizontal: 400.0),
-              child: Text(
+              padding: EdgeInsets.symmetric(
+                horizontal: MediaQuery.of(context).size.width > 600 ? 400.0 : 20.0),
+              child: const Text(
                 'Welcome to Union Shop!\n\nWe’re dedicated to giving you the very best University branded products, with a range of clothing and merchandise available to shop all year round! We even offer an exclusive personalisation service!\n\nAll online purchases are available for delivery or instore collection! \n\nWe hope you enjoy our products as much as we enjoy offering them to you. If you have any questions or comments, please don’t hesitate to contact us at hello@upsu.net. \n\nHappy shopping! \n\nThe Union Shop & Reception Team​​​​​​​​​.',
                 textAlign: TextAlign.left,
                 style: TextStyle(
